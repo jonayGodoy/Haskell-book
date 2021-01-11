c0f52c5abdafef6862b02ed9133e95e21aafe053
jonay.godoy.reyes <jonay.godoy.reyes@gmail.com>
2021-01-11 21:07:22 +0000

    where vs let

---
+++ Chapters/HelloHaskell/practiceWhere.hs
@@ -0,0 +1,17 @@
+mult1 = x * y
+ where x = 5
+       y = 6
+   
+exercise1 = x * 3 + y
+ where x = 3
+       y = 1000
+   
+   
+exercise2 = x * 5
+ where y = 10
+       x = 10 * 5 + y
+   
+exercise3 = z / x + y
+ where x = 7
+       y = negate x
+       z = y * 10
