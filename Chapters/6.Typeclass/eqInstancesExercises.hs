ac6c8c4a701f72c1b7ad53aee987ceb476d2d344
jonay.godoy.reyes <jonay.godoy.reyes@gmail.com>
2021-03-31 12:56:23 +0100

    finish exercise 6 and exercise 7

--- Chapters/6.Typeclass/eqInstancesExercises.hs
+++ Chapters/6.Typeclass/eqInstancesExercises.hs
@@ -13,14 +13,11 @@ instance Eq TisAnInteger where
 --1. Constructor de tipos
 --2. Constructor de datos
 
-
 --2 exercise
 data TwoIntegers = Two Integer Integer
 
 instance Eq TwoIntegers where
  (== )(Two a b) (Two a' b') = a == a' && b == b'
- 
- 
 
 --3 exercise
 data StringOrInt = TisAnInt Int | TisAString String
@@ -37,3 +34,22 @@ data Pair a = Pair a a
 instance Eq a => Eq (Pair a) where
  (==) (Pair x y) (Pair x' y') = x == x' && y == y'
 
+
+--exercise 5
+data Tuple a b = Tuple a b
+instance (Eq a, Eq b) => Eq (Tuple a b) where
+ (==) (Tuple a b) (Tuple a' b') = a == a' && b == b'
+
+-- exercise 6
+data Which a = ThisOne a | ThatOne a
+instance Eq a => Eq (Which a) where
+ (==) (ThisOne a) (ThisOne a') = a == a'
+ (==) (ThatOne b) (ThatOne b') = b == b'
+ (==) _ _ = False
+
+-- exercise 7
+data EitherOr a b = Hello a | Goodbye b
+instance (Eq a, Eq b) => Eq (EitherOr a b) where
+  Hello a == Hello a' = a == a'
+  Goodbye b == Goodbye b' = b == b'
+  _ == _ = False
