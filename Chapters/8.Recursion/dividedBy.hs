963cd45e7f676328a726a97d12f9ad24514ff42e
jonay.godoy.reyes <jonay.godoy.reyes@gmail.com>
2021-04-21 08:40:33 +0100

    my own division

---
+++ Chapters/8.Recursion/dividedBy.hs
@@ -0,0 +1,13 @@
+module DividedBy where
+
+type Numerator = Integer
+type Denominator = Integer
+type Quotient = Integer
+
+dividedBy :: Integral a => a -> a -> (a, a)
+dividedBy num denom = go num denom 0
+ where go n   d count
+        | n < d = (count, n)
+        | otherwise = go (n - d) d (count + 1)
+
+
