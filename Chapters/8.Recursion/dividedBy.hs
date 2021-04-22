7aed3581921ef73d74f8bb1e2f50b59c7decb4a2
Jonay Godoy <jonay.godoy.reyes@gmail.com>
2021-04-22 09:18:03 +0100

    dividedby v2

--- Chapters/8.Recursion/dividedBy.hs
+++ Chapters/8.Recursion/dividedBy.hs
@@ -1,13 +1,13 @@
 module DividedBy where
 
-type Numerator = Integer
-type Denominator = Integer
-type Quotient = Integer
+data DividedResult =  Result (Integer, Integer) | DividedByZero deriving (Show)
 
-dividedBy :: Integral a => a -> a -> (a, a)
-dividedBy num denom = go num denom 0
- where go n   d count
-        | n < d = (count, n)
-        | otherwise = go (n - d) d (count + 1)
+dividedBy :: Integer -> Integer -> DividedResult
+dividedBy num denom = go num denom 0 (denom < 0 && num < 0 || denom >= 0 && num >= 0) (num == 0 || denom == 0)
+ where go n d count sign hasZero
+        | hasZero = DividedByZero
+        | (abs n) < (abs d) && not sign = Result (-count, n)
+        | (abs n) < (abs d) && sign = Result (count, n)
+        | otherwise = go ((abs n) - (abs d)) d (count + 1) sign hasZero
 
 
