978a15abf09f2fa2e8f6b48f868ad037f8fabdde
Jonay Godoy <jonay.godoy.reyes@gmail.com>
2021-04-22 09:21:46 +0100

    dividedby v3

--- Chapters/8.Recursion/dividedBy.hs
+++ Chapters/8.Recursion/dividedBy.hs
@@ -3,11 +3,12 @@ module DividedBy where
 data DividedResult =  Result (Integer, Integer) | DividedByZero deriving (Show)
 
 dividedBy :: Integer -> Integer -> DividedResult
-dividedBy num denom = go num denom 0 (denom < 0 && num < 0 || denom >= 0 && num >= 0) (num == 0 || denom == 0)
- where go n d count sign hasZero
-        | hasZero = DividedByZero
+dividedBy 0 _ = DividedByZero
+dividedBy _ 0 = DividedByZero
+dividedBy num denom = go num denom 0 (denom < 0 && num < 0 || denom >= 0 && num >= 0)
+ where go n d count sign
         | (abs n) < (abs d) && not sign = Result (-count, n)
         | (abs n) < (abs d) && sign = Result (count, n)
-        | otherwise = go ((abs n) - (abs d)) d (count + 1) sign hasZero
+        | otherwise = go ((abs n) - (abs d)) d (count + 1) sign
 
 
