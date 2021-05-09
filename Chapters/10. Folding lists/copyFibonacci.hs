45cd19e8bb7933d7a41935973e0f734a0855bc3b
Jonay Godoy <jonay.godoy.reyes@gmail.com>
2021-05-09 08:34:13 +0100

    it finishes scanl exercises

--- Chapters/10. Folding lists/copyFibonacci.hs
+++ Chapters/10. Folding lists/copyFibonacci.hs
@@ -8,3 +8,10 @@ fibonacci x = fibonacci(x - 1) + fibonacci(x - 2)
 
 fibs = 1 : scanl (+) 1 fibs
 fibsN x = fibs !! x
+
+fibsJust20 = take 20 fibs
+
+--n * factorial (n - 1)
+factorials :: Integral a => a -> [a]
+factorials 0 = [1]
+factorials x = scanl (\x' y -> x * y) x (factorials (x - 1))
