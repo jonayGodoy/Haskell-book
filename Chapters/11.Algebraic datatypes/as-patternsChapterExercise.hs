7aff0eb9daa75840c92980bd19efca4f439dd6f7
Jonay Godoy <jonay.godoy.reyes@gmail.com>
2021-05-26 12:14:56 +0100

    finished capitalize parragraph exercise

--- Chapters/11.Algebraic datatypes/as-patternsChapterExercise.hs
+++ Chapters/11.Algebraic datatypes/as-patternsChapterExercise.hs
@@ -24,3 +24,33 @@ capitalizeWords = (map f ) . words
  where f t@(x:xs) = (t, (toUpper x : xs))
 
 
+--personal extra exercise, do words again
+words2 :: String -> [String]
+words2 [] = []
+words2 xs = [takeWhile (/=' ') xs] ++  words2 (dropWhile (==' ') $ dropWhile (/=' ') xs)
+
+takeWhile2 :: (Char -> Bool) -> String -> String
+takeWhile2 f (y:ys) 
+ | not (f y) = y : takeWhile2 f ys
+ | f y = ""
+ --- finished personal exercise
+
+
+capitalizeParagraph :: String -> String
+capitalizeParagraph =  plainText . plainParagraph . extractParagraph . capitalizeWords
+
+plainText xs = f $ concat xs
+ where f (x:[]) = x
+       f (x:xs) = x ++ " " ++ f xs
+
+plainParagraph = map (\(x:xs) -> snd x : (map (\(x',y') -> x') xs) )
+
+isLastPoint (x,y) = last x /= '.'
+
+extractParagraph :: [(String, String)] -> [[(String, String)]]
+extractParagraph [] = []
+extractParagraph xs = (takeWhile isLastPoint xs ++ [dropWhile isLastPoint xs !! 0]) 
+                        : extractParagraph (drop 1 (dropWhile isLastPoint xs))
+ 
+ 
+ 
