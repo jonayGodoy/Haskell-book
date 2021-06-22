6b89a500899de32ef71a21c0d14b218bd747bf9c
Jonay Godoy <jonay.godoy.reyes@gmail.com>
2021-06-22 19:36:04 +0100

    done Exercise: Maybe Another Monoid

--- Chapters/15. Monoid, Semigroup/test-project/test/Spec.hs
+++ Chapters/15. Monoid, Semigroup/test-project/test/Spec.hs
@@ -1,6 +1,8 @@
 import Test.Hspec
 import Test.QuickCheck
 
+data Optional a = Nada | Only a deriving (Eq, Show)
+
 type S = String
 type B = Bool
 
@@ -24,9 +26,33 @@ main = hspec $ do
    quickCheck (monoidLeftIdentity :: String -> Bool)
   it "has right identity" $ do
    quickCheck (monoidRightIdentity :: String -> Bool)
---exercise pending   
---  describe "Exercise: Maybe Another Monoid" $ do
---   it "quickCheck test" $ do
---    quickCheck (monoidAssoc :: FirstMappend)
---    quickCheck (monoidLeftIdentity :: FstId)
---    quickCheck (monoidRightIdentity :: FstId)FstId)
+  describe "Exercise: Maybe Another Monoid" $ do
+   it "quickCheck test" $ do
+    quickCheck (monoidAssoc :: FirstMappend)
+    quickCheck (monoidLeftIdentity :: FstId)
+    quickCheck (monoidRightIdentity :: FstId)
+
+
+newtype First' a = First' { getFirst' :: Optional a } deriving (Eq, Show)
+
+instance Monoid (First' a) where
+ mempty = First' Nada
+
+instance Semigroup (First' a) where
+ First' Nada <> (First' (Only x)) = First' (Only x)
+ (First' (Only x)) <> First' Nada = First' (Only x)
+ (First' (Only x)) <> (First' (Only y)) = First' (Only x)
+ First' Nada <> First' Nada = First' Nada
+ 
+firstMappend :: First' a -> First' a -> First' a
+firstMappend x y = mappend x y
+
+type FirstMappend = First' String -> First' String -> First' String
+ -> Bool
+ 
+type FstId = First' String -> Bool
+
+instance Arbitrary a => Arbitrary (First' a) where 
+ arbitrary = do 
+  x <- arbitrary
+  frequency [ (1, return (First' Nada)), (1, return (First' (Only x))) ]
