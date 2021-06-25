651e324602a2bacbad89372407be4f2ed8f91ded
Jonay Godoy <jonay.godoy.reyes@gmail.com>
2021-06-25 21:22:20 +0100

    done first and second semigroup exercises

---
+++ Chapters/15. Monoid, Semigroup/test-project/test/SemigroupChapterExercises.hs
@@ -0,0 +1,42 @@
+module SemigroupChapterExercises where
+
+import Data.Monoid
+import Test.QuickCheck
+
+-- 1 .
+
+data Trivial = Trivial deriving (Eq, Show)
+
+instance Semigroup Trivial where
+ _ <> _ = Trivial
+ 
+instance Arbitrary Trivial where
+ arbitrary = return Trivial
+
+semigroupAssoc :: (Eq m, Semigroup m) => m -> m -> m -> Bool
+semigroupAssoc a b c = (a <> (b <> c)) == ((a <> b) <> c)
+
+type TrivAssoc = Trivial -> Trivial -> Trivial -> Bool
+
+
+--2 .
+newtype Identity a = Identity a deriving (Show, Eq)
+
+
+instance Arbitrary a => Arbitrary (Identity a) where
+ arbitrary = do
+  x <- arbitrary
+  return (Identity x)
+
+
+type IdentityAssoc = Identity Sum -> Identity Sum -> Identity Sum -> Bool
+
+instance Semigroup a => Semigroup (Identity a) where
+ (Identity x) <> (Identity y) = Identity (x <> y)
+
+main :: IO ()
+main = do
+ --1.
+  quickCheck (semigroupAssoc :: TrivAssoc)
+ --2.
+  quickCheck (semigroupAssoc :: IdentityAssoc)
