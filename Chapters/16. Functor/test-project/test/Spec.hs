52b58b7209889d4977a4f7fd83e2171e19f6c88a
Jonay Godoy <jonay.godoy.reyes@gmail.com>
2021-07-07 12:34:13 +0100

    create first function generator

--- Chapters/16. Functor/test-project/test/Spec.hs
+++ Chapters/16. Functor/test-project/test/Spec.hs
@@ -1,5 +1,6 @@
 import Test.Hspec
 import Test.QuickCheck
+import Test.QuickCheck.Function
 
 -- test functor laws
 
@@ -10,13 +11,21 @@ functorIdentity f = fmap id f == f
 functorCompose :: (Eq (f c), Functor f) => (a -> b) -> (b -> c) -> f a -> Bool
 functorCompose f g x = (fmap g (fmap f x)) == (fmap (g . f) x)
 
+functorCompose' :: (Eq (f c), Functor f) => f a -> Fun a b -> Fun b c -> Bool
+functorCompose' x (Fun _ f) (Fun _ g) = (fmap (g . f) x) == (fmap g . fmap f $ x)
+
 f :: [Int] -> Bool
 f x = functorIdentity x
 
 c = functorCompose (+1) (*2)
 li x = c (x :: [Int])
 
+type IntToInt = Fun Int Int
+type IntFC = [Int] -> IntToInt -> IntToInt -> Bool
+
+
 main :: IO ()
 main = do
  quickCheck f
  quickCheck li
+ quickCheck (functorCompose' :: IntFC)
