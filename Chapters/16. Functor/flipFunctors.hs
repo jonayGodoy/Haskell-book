173053b3039e4e8ca995f0cf87f28130bc43622e
Jonay Godoy <jonay.godoy.reyes@gmail.com>
2021-07-19 10:07:27 +0100

    example of flip funtors, each functor to each type

---
+++ Chapters/16. Functor/flipFunctors.hs
@@ -0,0 +1,11 @@
+{-# LANGUAGE FlexibleInstances #-}
+--Allow a type parameter to occure twice within a type class instance.
+module FlipFunctors where
+
+
+data Tuple a b = Tuple a b deriving (Eq, Show)
+
+newtype Flip f a b = Flip (f b a) deriving (Eq, Show)
+
+instance Functor (Flip Tuple a) where
+ fmap f (Flip (Tuple a b)) = Flip $ Tuple (f a) b
