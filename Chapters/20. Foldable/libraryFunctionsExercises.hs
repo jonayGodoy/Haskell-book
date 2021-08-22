5b69346df31a35fea9da9b144384353a27496962
Jonay Eliezer Godoy Reyes <jonaydam2@gmail.com>
2021-08-22 14:39:25 +0100

    i carry on with library exercises

--- Chapters/20. Foldable/libraryFunctionsExercises.hs
+++ Chapters/20. Foldable/libraryFunctionsExercises.hs
@@ -5,3 +5,32 @@ import Data.Monoid
 
 sum' :: (Foldable t, Num a) => t a -> a
 sum' xs = getSum $ foldMap Sum xs
+
+
+product' :: (Foldable t, Num a) => t a -> a
+product' xs = getProduct $ foldMap Product xs
+
+
+elem' :: (Foldable t, Eq a) => a -> t a -> Bool
+elem' x' xs = foldr (\x y -> x == x') False xs
+
+
+minimum' :: (Foldable t, Ord a) => t a -> Maybe a
+minimum' = foldr go Nothing
+  where
+    go x Nothing  = Just x
+    go x (Just y) = Just (min x y)
+
+
+maximum' :: (Foldable t, Ord a) => t a -> Maybe a
+maximum' = foldr go Nothing
+  where
+    go x Nothing  = Just x
+    go x (Just y) = Just (max x y)
+
+
+null' :: (Foldable t) => t a -> Bool
+null' = foldr (\_ _ -> False) True
+
+length' :: (Foldable t) => t a -> Int
+length' = foldr (\x y -> y + 1) 0
