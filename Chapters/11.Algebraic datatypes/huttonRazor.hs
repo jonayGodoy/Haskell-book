aceafaff20e75d3172ae37f7a8d5ab36f972b381
Jonay Eliezer Godoy Reyes <jonaydam2@gmail.com>
2021-10-04 19:08:50 +0100

    example of trifecta use

--- Chapters/11.Algebraic datatypes/huttonRazor.hs
+++ Chapters/11.Algebraic datatypes/huttonRazor.hs
@@ -1,14 +1,16 @@
 module HuttonRazor where
 
 
-data Expr = Lit Integer | Add Expr Expr
+data Expr = Lit Integer | Add Expr Expr | Substract Expr Expr
 
 
 eval :: Expr -> Integer
 eval (Lit x) = x
-eval (Add x y) = (eval x) +  (eval y)
+eval (Add x y) = (eval x) + (eval y)
+eval (Substract x y) = (eval x) - (eval y)
 
 
 printExpr :: Expr -> String
 printExpr (Lit x) = show x
 printExpr (Add x y) = (printExpr x) ++ "+" ++ (printExpr y)
+printExpr (Substract x y) = (printExpr x) ++ "-" ++ (printExpr y)
